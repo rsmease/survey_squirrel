c9c3aca955e7b5248e43a961ca32ca27a77f2066
rsmease <rsmease@gmail.com>
2019-04-21 15:46:11 -0700

    Update root, lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: {format: JSON} do
-    resources :users, only: [:create, :show]
-    resource :session, only: [ :new, :create, :destroy ]
 
-    resources :surveys, only: [:index, :create, :show]
+  root 'api/surveys#index'
+
+  namespace :api, defaults: { format: JSON } do
+    resources :users, only: %i[create show]
+    resource :session, only: %i[new create destroy]
+
+    resources :surveys, only: %i[index create show]
   end
 end
