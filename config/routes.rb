4b55284e923899a86f4bb99405279faf8e1813c8
rsmease <rsmease@gmail.com>
2019-04-21 06:47:48 -0700

    add session routes to API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: JSON} do
     resources :users, only: [:create, :show]
 
-    #resource :session, only: [ :new, :create, :destroy ]
+    resource :session, only: [ :new, :create, :destroy ]
   end
 end
