e362d2076ff26e49faf0938c23df5a33b925312d
rsmease <rsmease@gmail.com>
2019-04-21 15:42:24 -0700

    Linting, update format_question

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,18 +5,19 @@ class Survey < ApplicationRecord
   after_initialize :format_question
 
   belongs_to :surveyor,
-    class_name: 'User',
-    foreign_key: 'surveyor_id',
-    primary_key: 'id',
-    dependent: :destroy,
-    optional: true
+             class_name: 'User',
+             foreign_key: 'surveyor_id',
+             primary_key: 'id',
+             dependent: :destroy,
+             optional: true
 
   private
 
   def format_question
-    return unless self.question
+    return unless question && !question.empty?
 
-    self.question.last == '?' || self.question += '?'
-    self.question = self.question.split(" ").map(&:downcase).map(&:capitalize).join(" ")
+    question.last == '?' || self.question += '?'
+    self.question =
+      self.question.split(' ').map(&:downcase).map(&:capitalize).join(' ')
   end
 end
