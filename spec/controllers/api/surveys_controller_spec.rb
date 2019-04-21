88e855c828cd26eec132fc133a063411ded4bb03
rsmease <rsmease@gmail.com>
2019-04-21 15:42:39 -0700

    Update controller specs

--- spec/controllers/api/surveys_controller_spec.rb
+++ spec/controllers/api/surveys_controller_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Api::SurveysController, type: :controller do
     it { should respond_with(200) }
     it { should render_template('api/surveys/index') }
   end
+
+  describe '#create' do
+    context 'when creating a valid survey' do
+      before { post :create, params: { survey: attributes_for(:survey) } }
+      it { should respond_with(200) }
+      it { should render_template('api/surveys/show') }
+    end
+
+    context 'when creating an ivalid survey with no question' do
+      before do
+        post :create, params: {
+          survey: attributes_for(:survey, question: '')
+        }
+      end
+
+      it { should respond_with(422) }
+      it { should_not render_template('api/surveys/index') }
+    end
+  end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
   describe '#create' do
-    context 'with a normal user' do
+    context 'when creating a valid user' do
       before { post :create, params: { user: attributes_for(:user) } }
       it { should respond_with(200) }
       it { should set_session }
     end
 
     context 'with a user that has no password' do
-      before { post :create, params: { user: attributes_for(:user, password: "") } }
+      before { post :create, params: { user: attributes_for(:user, password: '') } }
       it { should respond_with(422) }
       it { should_not set_session }
     end
