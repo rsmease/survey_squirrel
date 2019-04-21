f52a7ba7bf78f721acb174fd7d2c615d52960bef
rsmease <rsmease@gmail.com>
2019-04-21 16:21:37 -0700

    add draft of survey responses controller

--- app/controllers/api/survey_responses_controller.rb
+++ app/controllers/api/survey_responses_controller.rb
@@ -1,2 +1,18 @@
 class Api::SurveyResponsesController < ApplicationController
+  def create
+    @response = SurveyReponse.create(response_params)
+
+    if @response.save
+      @survey = Survey.find(response_params[:survey_id])
+      render 'api/surveys/show'
+    else
+      render json: @response.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def survey_response_params
+    params.require(:survey_response).permit(:response, :survey_id)
+  end
 end
