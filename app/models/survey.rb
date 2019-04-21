3d06af4bfa8b4fb6f2553273c39bc042aa975197
rsmease <rsmease@gmail.com>
2019-04-21 08:19:15 -0700

    add model validations for question

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,10 @@
 class Survey < ApplicationRecord
+  validate :question, presence: true
+  after_initialize :format_question
+
+  def format_question
+    if self.question.last !== '?'
+      self.question += '?'
+    end
+  end
 end
