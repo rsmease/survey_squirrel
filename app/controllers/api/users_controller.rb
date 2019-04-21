b249ce5f42ee9184eca31d6e5096b6b5e754638e
rsmease <rsmease@gmail.com>
2019-04-21 06:46:30 -0700

    Add users controller under API

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      #not sure what this should be just yet
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "api/users/show"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :bio, :profile_image_url)
+  end
+end
